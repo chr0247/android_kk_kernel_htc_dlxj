53b53d13271b090e7462e31f39f61de2089a07c0
schqiushui <orochi9999@gmail.com>
2014-02-06 03:31:37 +0800

    firstcommit

--- README.md
+++
@@ -1,4 +0,0 @@
-android_kk_kernel_htc_dlxj
-==========================
-
-android_kk_kernel_htc_dlxj(HTL21)
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Thu Feb 6 01:08:57 CST 2014"
+#define LINUX_COMPILE_BY "schqiushui"
+#define LINUX_COMPILE_HOST "Lenovo-M490"
+#define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
