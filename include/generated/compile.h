b1cf8ac3611a8d03d8f0bfa420f90c97bba43fc5
schqiushui <orochi9999@gmail.com>
2014-02-12 03:22:31 +0800

    fix fastcharge

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Sun Feb 9 06:18:50 CST 2014"
+#define UTS_VERSION "#7 SMP PREEMPT Wed Feb 12 03:19:12 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
