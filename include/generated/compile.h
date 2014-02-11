ed3b80b7dd9b2e5579733137f5bce147ddd97f2a
schqiushui <orochi9999@gmail.com>
2014-02-12 04:36:42 +0800

    add:Sound Control: Sound control for WCD93xx codec

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Wed Feb 12 03:58:40 CST 2014"
+#define UTS_VERSION "#11 SMP PREEMPT Wed Feb 12 04:34:24 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
