890a2fad1170b12fc895feb53e201740fbea05a0
schqiushui <orochi9999@gmail.com>
2014-02-13 04:34:19 +0800

    deluxe_j_defconfig update

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 13 03:00:23 CST 2014"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 13 04:28:37 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
