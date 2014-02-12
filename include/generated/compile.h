88e0ba118564c5daa878eed4cece6fa3a8fb38c2
schqiushui <orochi9999@gmail.com>
2014-02-13 02:35:27 +0800

    fix:MSM LCD temperature calibrations

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Feb 13 02:28:06 CST 2014"
+#define LINUX_COMPILE_BY "schqiushui"
+#define LINUX_COMPILE_HOST "Lenovo-M490"
+#define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
