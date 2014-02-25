443b22b0e6774f837561069e1a495a26a0cba072
schqiushui <orochi9999@gmail.com>
2014-02-25 16:00:24 +0800

    add support for kernel mode NEON & crypto: add NEON accelerated XOR implementation

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 13 04:28:37 CST 2014"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Feb 25 23:47:18 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
