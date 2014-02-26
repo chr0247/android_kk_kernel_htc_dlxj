0fbdeeae4016e514a99dd1d8794da8292d5c876a
schqiushui <orochi9999@gmail.com>
2014-02-26 22:20:50 +0800

    Tweak GPU

--- arch/arm/mach-msm/board-deluxe_j-gpu.c
+++ arch/arm/mach-msm/board-deluxe_j-gpu.c
@@ -228,43 +228,73 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 #ifdef CONFIG_GPU_OVERCLOCK
-		{
-#ifdef CONFIG_GPU_OVERCLOCK_450
-			.gpu_freq = 450000000,
+                {
+                        .gpu_freq = 487500000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 450000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 400000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 320000000,
+                        .bus_freq = 3,
+                        .io_fraction = 33,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 2,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 128000000,
+                        .bus_freq = 1,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 27000000,
+                        .bus_freq = 0,
+                },
 #else
-			.gpu_freq = 487500000,
+{
+                        .gpu_freq = 400000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 320000000,
+                        .bus_freq = 3,
+                        .io_fraction = 33,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 2,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 27000000,
+                        .bus_freq = 0,
+                },
 #endif
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
+        },
+#ifdef CONFIG_GPU_OVERCLOCK
+        .init_level = 5,
+        .num_levels = 7,
 #else
-		{
-			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
+        .init_level = 2,
+        .num_levels = 4,
 #endif
-		{
-			.gpu_freq = 325000000,
-			.bus_freq = 3,
-			.io_fraction = 33,
-		},
-		{
-			.gpu_freq = 200000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
-		},
-		{
-			.gpu_freq = 27000000,
-			.bus_freq = 0,
-		},
-	},
-	.init_level = 2,
-	.num_levels = 4,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
-	.strtstp_sleepwake = true,
+	.strtstp_sleepwake = false,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp3d_bus_scale_pdata,
