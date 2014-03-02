89810dd5afcb045bac17cf1b10879f4ed72ef728
schqiushui <orochi9999@gmail.com>
2014-03-02 18:37:25 +0800

    fix error config

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 2 17:01:27 CST 2014"
+#define LINUX_COMPILE_BY "schqiushui"
+#define LINUX_COMPILE_HOST "Lenovo-M490"
+#define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (Linaro GCC 4.8-2014.01) "
