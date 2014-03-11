ea1acfeee1e6f4444d4c6f5d1cf5390fe977e353
schqiushui <orochi9999@gmail.com>
2014-03-12 01:56:42 +0800

    more updates

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 21 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 SMP PREEMPT Sat Mar 8 23:32:14 CST 2014"
+#define UTS_VERSION "#21 SMP PREEMPT Wed Mar 12 01:53:32 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140203 (prerelease) (Linaro GCC 4.8-2014.02) "
