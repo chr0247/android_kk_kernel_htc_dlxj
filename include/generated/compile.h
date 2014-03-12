b155e5cf3469e82b9ac19e59b0f26b4f3a601474
schqiushui <orochi9999@gmail.com>
2014-03-13 03:22:23 +0800

    some updates

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 25 */
+/* This file is auto generated, version 26 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#25 SMP PREEMPT Thu Mar 13 01:38:35 CST 2014"
+#define UTS_VERSION "#26 SMP PREEMPT Thu Mar 13 03:18:34 CST 2014"
 #define LINUX_COMPILE_BY "schqiushui"
 #define LINUX_COMPILE_HOST "Lenovo-M490"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (crosstool-NG 1.19.0) "
